8c83252e5b7d8173b8b14f4a35328875a7f27eff
Sung Won Chung <sungwonchung3@gmail.com>
2024-07-02 10:04:27 -0700

    add state

--- config.py
+++ config.py
@@ -1,5 +1,25 @@
 from pathlib import Path
+import os
 
 from sqlmesh.dbt.loader import sqlmesh_config
+from sqlmesh.core.config import PostgresConnectionConfig
 
-config = sqlmesh_config(Path(__file__).parent)
+config = sqlmesh_config(
+    project_root=Path(__file__).parent,
+    state_connection=PostgresConnectionConfig(
+        host=os.getenv("SQLMESH_STATE_HOST"),
+        port=5432,
+        user=os.getenv("SQLMESH_STATE_USERNAME"),
+        password=os.getenv("SQLMESH_STATE_PASSWORD"),
+        database="sqlmesh_state_snowbricks_demo",
+    ),
+)
+
+# yaml equivalent
+# state_connection:
+#     type: postgres
+#     host: {{ env_var('SQLMESH_STATE_HOST') }}
+#     port: 5432
+#     user: {{ env_var('SQLMESH_STATE_USERNAME') }}
+#     password: {{ env_var('SQLMESH_STATE_PASSWORD') }}
+#     database: sqlmesh_state_snowbricks_demo
