0775b37d9784ce8c4fc9dea63069b1e48faea9bc
Sung Won Chung <sungwonchung3@gmail.com>
2024-07-03 09:04:01 -0700

    cleanup

--- config.py
+++ config.py
@@ -5,18 +5,6 @@ from sqlmesh.dbt.loader import sqlmesh_config
 from sqlmesh.core.config import PostgresConnectionConfig
 
 # databricks
-# config = sqlmesh_config(
-#     project_root=Path(__file__).parent,
-#     state_connection=PostgresConnectionConfig(
-#         host=os.getenv("SQLMESH_STATE_HOST"),
-#         port=5432,
-#         user=os.getenv("SQLMESH_STATE_USERNAME"),
-#         password=os.getenv("SQLMESH_STATE_PASSWORD"),
-#         database="sqlmesh_state_snowbricks_demo",
-#     ),
-# )
-
-# bigquery
 config = sqlmesh_config(
     project_root=Path(__file__).parent,
     state_connection=PostgresConnectionConfig(
@@ -24,9 +12,21 @@ config = sqlmesh_config(
         port=5432,
         user=os.getenv("SQLMESH_STATE_USERNAME"),
         password=os.getenv("SQLMESH_STATE_PASSWORD"),
-        database="sqlmesh_state_migrate_bigquery_demo",
+        database="sqlmesh_state_snowbricks_demo",
     ),
 )
+
+# bonus: bigquery
+# config = sqlmesh_config(
+#     project_root=Path(__file__).parent,
+#     state_connection=PostgresConnectionConfig(
+#         host=os.getenv("SQLMESH_STATE_HOST"),
+#         port=5432,
+#         user=os.getenv("SQLMESH_STATE_USERNAME"),
+#         password=os.getenv("SQLMESH_STATE_PASSWORD"),
+#         database="sqlmesh_state_migrate_bigquery_demo",
+#     ),
+# )
 
 # yaml equivalent
 # state_connection:
